6ddac209d5852d03f7f19a2cda11e53566c5e0ad
Dmitry Ovchinnikov <dmitry.o.ovchinnikov@gmail.com>
2023-12-25 09:30:35 +0400

    Make refactoring.

--- src/main/java/com/oodmi/notes/model/Note.java
+++ src/main/java/com/oodmi/notes/model/Note.java
@@ -1,7 +1,5 @@
 package com.oodmi.notes.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -15,8 +13,6 @@ import java.util.List;
 @Data
 @Accessors(chain = true)
 @Document("notes")
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Note {
 
     @MongoId
